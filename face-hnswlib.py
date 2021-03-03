4a7ff84fc22da07d695d40afc02185af0900e1e1
Rickitik <romeo2310@yandex.ru>
2021-03-03 09:32:28 +0300

    train function changed for using hnswlib model

--- face-hnswlib.py
+++ face-hnswlib.py
@@ -76,10 +76,10 @@ class Index:
         return labels, distances
 
 
-def train(train_dir, model_save_path=None, n_neighbors=1, knn_algo='ball_tree', verbose=False):
+def train(train_dir, model_save_path=None, num_threads=4, verbose=False):
     """
     Trains a knn classifier for face recognition.
-    :param train_dir: directory that contains a sub-directory for each known person, with its name.
+    :param train_dir: directory that contains a sub-directory for each known person, with its name coded by DIGITS.
      Structure:
         <train_dir>/
         ├── <person1>/
@@ -91,9 +91,8 @@ def train(train_dir, model_save_path=None, n_neighbors=1, knn_algo='ball_tree', 
         │   └── <somename2>.jpeg
         └── ...
     :param model_save_path: (optional) path to save model on disk
-    :param n_neighbors: (optional) number of neighbors to weigh in classification. Chosen automatically if not specified
-    :param knn_algo: (optional) underlying data structure to support knn.default is ball_tree
-    :param verbose: verbosity of training
+    :param num_threads: Set number of threads used during batch search/construction
+    :param verbose: verbose mode for training
     :return: returns knn classifier that was trained on the given data.
     """
     X = []
@@ -118,25 +117,20 @@ def train(train_dir, model_save_path=None, n_neighbors=1, knn_algo='ball_tree', 
                 X.append(face_recognition.face_encodings(image, known_face_locations=face_bounding_boxes)[0])
                 y.append(class_dir)
 
-    # Determine how many neighbors to use for weighting in the KNN classifier
-    if n_neighbors is None:
-        n_neighbors = int(round(math.sqrt(len(X))))
-        if verbose:
-            print("Chose n_neighbors automatically:", n_neighbors)
-
     # Create and train the KNN classifier
-    # TODO Define class and fitting
-    knn_clf = neighbors.KNeighborsClassifier(n_neighbors=n_neighbors, algorithm=knn_algo, weights='distance')
-    knn_clf.fit(X, y)
+    model = Index(space='l2', dim=128)
+    model.init_index(len(X))
+    model.set_ef(10)
+    model.set_num_threads(num_threads)
+    model.add_items(X, y)
 
     # Save the trained KNN classifier
     if model_save_path is not None:
-        with open(model_save_path, 'wb') as f:
-            pickle.dump(knn_clf, f)
+        model.save_index(model_save_path)
 
-    return knn_clf
+    return model
 
-
+#TODO: change prediction function
 def predict(X_frame, knn_clf=None, model_path=None, distance_threshold=0.5):
     """
     Recognizes faces in given image using a trained KNN classifier
