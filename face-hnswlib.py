5093e3d688cb8525aff85f2d9ce8a965dc419b50
Rickitik <romeo2310@yandex.ru>
2021-03-04 10:55:58 +0300

    predict function added

--- face-hnswlib.py
+++ face-hnswlib.py
@@ -142,12 +142,11 @@ def load_model(model, model_path):
     model.load_index(model_path)
 
 
-def predict(X_frame, model=None):
+def predict(model, X_frame, distance_threshold=0.6):
     """
     Recognizes faces in given image using a trained KNN classifier
     :param X_frame: frame to do the prediction on.
-    :param knn_clf: (optional) a knn classifier object. if not specified, model_save_path must be specified.
-    :param model_path: (optional) path to a pickled knn classifier. if not specified, model_save_path must be knn_clf.
+    :param model: a knn classifier object.
     :param distance_threshold: (optional) distance threshold for face classification. the larger it is, the more chance
            of mis-classifying an unknown person as a known one.
     :return: a list of names and face locations for the recognized faces in the image: [(name, bounding box), ...].
@@ -155,7 +154,6 @@ def predict(X_frame, model=None):
     """
     if model is None:
         raise Exception("Must supply knn_clf")
-
 
     X_face_locations = face_recognition.face_locations(X_frame)
 
@@ -167,11 +165,13 @@ def predict(X_frame, model=None):
     faces_encodings = face_recognition.face_encodings(X_frame, known_face_locations=X_face_locations)
 
     # Use the KNN model to find the best matches for the test face
-    closest_distances = knn_clf.kneighbors(faces_encodings, n_neighbors=1)
-    are_matches = [closest_distances[0][i][0] <= distance_threshold for i in range(len(X_face_locations))]
+    labels, distances = model.knn_query(faces_encodings, k=1)
+    # TODO: type of answer investigate
+    are_matches = [distances[i] <= distance_threshold for i in range(len(X_face_locations))]
 
     # Predict classes and remove classifications that aren't within the threshold
-    return [(pred, loc) if rec else ("unknown", loc) for pred, loc, rec in zip(knn_clf.predict(faces_encodings), X_face_locations, are_matches)]
+    return [(pred, loc) if rec else ("unknown", loc) for pred, loc, rec in zip(labels, X_face_locations, are_matches)]
+
 
 
 def show_prediction_labels_on_image(frame, predictions):
